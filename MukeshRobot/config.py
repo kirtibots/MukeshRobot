cedc95d520e8f9a44f845a9287ecf779ea221bb5
- ᴋɪʀᴛɪ ʙоᴛѕ [ 🇮🇳 <kritikumari8665@gmail.com>
2026-08-19 16:27:34 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -1,9 +1,9 @@
 class Config(object):
-    LOGGER = True
+    LOGGER = "-1003979103138"
     API_ID =None 
     API_HASH = ""
     TOKEN = ""  
-    OWNER_ID= "8857291657"
+    OWNER_ID = "8857291657"
     
     SUPPORT_CHAT = "https://t.me/annu_support" 
     START_IMG = "https://d.uguu.se/CHvqjpqC.jpg"
