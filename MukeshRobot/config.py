4b6eff1239d762936d27e30a0a00db4c386de0e6
- ᴋɪʀᴛɪ ʙоᴛѕ [ 🇮🇳 <kritikumari8665@gmail.com>
2026-08-19 16:36:18 +0530

    Fix formatting and remove extra parentheses in config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -1,28 +1,27 @@
 class Config(object):
-    LOGGER = "-1003979103138")
-    API_ID =None 
+    LOGGER = "-1003979103138"
+    API_ID = None
     API_HASH = ""
-    TOKEN = ""  
-    OWNER_ID = "8857291657")
-    
-    SUPPORT_CHAT = "https://t.me/annu_support") 
-    START_IMG = "https://d.uguu.se/CHvqjpqC.jpg")
+    TOKEN = ""
+    OWNER_ID = "8857291657"
+
+    SUPPORT_CHAT = "https://t.me/annu_support"
+    START_IMG = "https://d.uguu.se/CHvqjpqC.jpg"
     EVENT_LOGS = ()
-    MONGO_DB_URI= ""
-   
+    MONGO_DB_URI = ""
+
     DATABASE_URL = ""  # A sql database url from elephantsql.com
     CASH_API_KEY = (
         ""
     )
     TIME_API_KEY = ""
 
-    
-    BL_CHATS = [] 
+    BL_CHATS = []
     DRAGONS = []
-    DEV_USERS = []  
-    DEMONS = [] 
-    TIGERS = []  
-    WOLVES = [] 
+    DEV_USERS = []
+    DEMONS = []
+    TIGERS = []
+    WOLVES = []
 
     ALLOW_CHATS = True
     ALLOW_EXCL = True
@@ -33,7 +32,7 @@ class Config(object):
     STRICT_GBAN = True
     TEMP_DOWNLOAD_DIRECTORY = "./"
     WORKERS = 8
-    
+
 
 class Production(Config):
     LOGGER = True
