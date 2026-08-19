1d26d5c5d95130009b6b6714e21f949f474184eb
- ᴋɪʀᴛɪ ʙоᴛѕ [ 🇮🇳 <kritikumari8665@gmail.com>
2026-08-19 16:28:59 +0530

    Fix syntax errors in config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -1,12 +1,12 @@
 class Config(object):
-    LOGGER = "-1003979103138"
+    LOGGER = "-1003979103138")
     API_ID =None 
     API_HASH = ""
     TOKEN = ""  
-    OWNER_ID = "8857291657"
+    OWNER_ID = "8857291657")
     
-    SUPPORT_CHAT = "https://t.me/annu_support" 
-    START_IMG = "https://d.uguu.se/CHvqjpqC.jpg"
+    SUPPORT_CHAT = "https://t.me/annu_support") 
+    START_IMG = "https://d.uguu.se/CHvqjpqC.jpg")
     EVENT_LOGS = ()
     MONGO_DB_URI= ""
    
