d90fc02216768963e301e23b696f0c839d90f569
- ᴋɪʀᴛɪ ʙоᴛѕ [ 🇮🇳 <kritikumari8665@gmail.com>
2026-08-19 16:58:52 +0530

    Update button text from 'ɴᴏᴏʙ' to 'ᴏᴡᴇɴʀ'

--- MukeshRobot/modules/alive.py
+++ MukeshRobot/modules/alive.py
@@ -44,7 +44,7 @@ PHOTO = [
 
 Mukesh = [
     [
-        InlineKeyboardButton(text="ɴᴏᴏʙ", user_id=OWNER_ID),
+        InlineKeyboardButton(text="ᴏᴡᴇɴʀ", user_id=OWNER_ID),
         InlineKeyboardButton(text="ꜱᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{SUPPORT_CHAT}"),
     ],
     [
