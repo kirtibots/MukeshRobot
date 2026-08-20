304cb3d272c1d13beb4b01629b478037d376a4cb
- ᴋɪʀᴛɪ ʙоᴛѕ [ 🇮🇳 <kritikumari8665@gmail.com>
2026-08-20 07:57:05 +0530

    Fix button text in InlineKeyboardButton

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -84,7 +84,7 @@ PM_START_TEXT = """
 buttons = [
     [
         
-        InlineKeyboardButton(text="🛡️", callback_data="mukesh_"),
+        InlineKeyboardButton(text="👇", callback_data="mukesh_"),
         InlineKeyboardButton(text="💳", callback_data="source_"),
         InlineKeyboardButton(text="🧑‍💻", url=f"tg://user?id={OWNER_ID}"),
         InlineKeyboardButton(text="🖥️", callback_data="Main_help"),
