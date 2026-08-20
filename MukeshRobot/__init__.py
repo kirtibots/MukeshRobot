71e5ddffc7d9dfa5a0cebe0e2bd9dd46bb9b93fd
- ᴋɪʀᴛɪ ʙоᴛѕ [ 🇮🇳 <kritikumari8665@gmail.com>
2026-08-20 07:39:03 +0530

    Update environment variables for support chat and dev users

--- MukeshRobot/__init__.py
+++ MukeshRobot/__init__.py
@@ -49,8 +49,8 @@ if ENV:
         "START_IMG", ""
     )
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "worldwide_friend_zone")
-    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "annu_updates")
+    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("annu_updates", "./")
     TOKEN = os.environ.get("TOKEN", None)
     TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
     WORKERS = int(os.environ.get("WORKERS", 8))
@@ -67,7 +67,7 @@ if ENV:
 
     try:
         DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
-        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "2145093972").split())
+        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "8857291657").split())
     except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
 
